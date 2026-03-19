f4497744b30e4a93a38f7c25217cf2fd1838deef
Guillermo Callaghan <guillermo.callaghan@huawei.com>
2026-03-19 19:37:15 +0000

    [HUAWEI] Handle constant folding to avoid creating of constant when it is based on a dynamic dimension.

--- tensorflow/core/common_runtime/constant_folding.cc
+++ tensorflow/core/common_runtime/constant_folding.cc
@@ -437,6 +437,24 @@ void AddNodeToConstantGraph(
   }
 }
 
+bool GetShapeFromArgNode(const Node* node, TensorShapeProto* out_shape) {
+  for (const Edge* edge : node->in_edges()) {
+    if (edge->IsControlEdge()) continue;
+
+    Node* input_node = edge->src();
+    if (input_node->type_string() == "_Arg") {
+      std::vector<TensorShapeProto> shapes_vector;
+      if (GetNodeAttr(input_node->def(), "_output_shapes", &shapes_vector)
+              .ok() &&
+          !shapes_vector.empty()) {
+        *out_shape = shapes_vector[0];
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 // Replaces constant-foldable shape node n by a vector of constants in
 // constant_graph, which is being built up for subsequent evaluation of constant
 // propagation. node_map is the mapping of nodes in the original graph to nodes
@@ -449,12 +467,21 @@ void AddShapeNodeToConstantGraph(
         shape_replacement_map,
     std::unordered_map<Node*, std::vector<Node*>>* node_map,
     const ConstantFoldNameGenerator& generate_new_name, Graph* constant_graph) {
+
+  TensorShapeProto user_inferred_shape;
+  bool hasDynamic = false;
+  if (GetShapeFromArgNode(n, &user_inferred_shape)) {
+    hasDynamic = true;
+  }
+
   std::vector<Node*>& added = (*node_map)[n];
   const string& node_name = n->name();
   for (const Tensor& t : shape_replacement_map.at(n)) {
     auto builder =
         NodeDefBuilder(generate_new_name(constant_graph, node_name), "Const")
             .Attr("dtype", t.dtype())
+            .Attr("hasDynamic", hasDynamic)
+            .Attr("user_inferred_shape", user_inferred_shape)
             .Attr("value", t);
     NodeDef def;
     CHECK(builder.Finalize(&def).ok());
@@ -575,8 +602,16 @@ bool ReplaceTensorWithConstant(
   }
   const string& node_name = n->name();
   Node* constant_node;
+
+  TensorShapeProto user_inferred_shape;
+  bool hasDynamic = false;
+  if (GetShapeFromArgNode(tensor.first, &user_inferred_shape)) {
+    hasDynamic = true;
+  }
   auto builder = NodeDefBuilder(generate_new_name(graph, node_name), "Const")
                      .Attr("dtype", constant.dtype())
+                     .Attr("hasDynamic", hasDynamic)
+                     .Attr("user_inferred_shape", user_inferred_shape)
                      .Attr("value", constant);
   if (partition_device) {
     builder.Device(partition_device->name());
